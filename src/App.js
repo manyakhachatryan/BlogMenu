7bc88a0550172a48d0392837a0aa7a9d5200bd68
Manya Khachatryan <manykhac@c2r17s2.42yerevan.am>
2023-04-24 14:53:29 +0400

    login, register components and  page routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import Home from "./components/pages/home/Home.js"
+import Login from "./components/pages/login/Login.js";
+import Register from "./components/pages/register/register.js";
+import Settings from "./components/pages/settings/Settings.js";
 import Single from "./components/pages/single/Single.js";
 import Write from "./components/pages/write/Write.js";
 import TopBar from "./components/topbar/TopBar.js";
 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+
 function App() {
+  const user = false;
   return (
-    <>
-    <TopBar/>
-    <Write/>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/register" element={user? <Home/> : <Register />} />
+        <Route path="/login" element={user? <Home/> : <Login/>} />
+        <Route path="/post/:postId" element={<Single/>} />
+        <Route path="/write" element={user ? <Write/> : <Register />} />
+        <Route path="/settings" element={user ? <Settings/> : <Register/>} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
- 
+export default App; 
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,11 +1,13 @@
 import Header from "../../header/Header" 
 import Posts from "../../posts/Posts"
 import Sidebar from "../../sidebar/Sidebar"
+import Topbar from '../../topbar/TopBar.js'
 import "./home.css"
 
 export default function Home (){
     return (
       <>
+      <Topbar/>
       <Header/>
       <div className="home">
           <Posts/>
